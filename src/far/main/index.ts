7347431db35cf22a66a6e48f76ef002c1c5b77d7
worksapce <289582304@qq.com>
2025-01-17 14:08:14 +0800

    debugger:调试

--- src/far/main/index.ts
+++ src/far/main/index.ts
@@ -47,6 +47,7 @@ async function createWindow() {
   win = new BrowserWindow({
     title: "Main window",
     icon: path.join(process.env.VITE_PUBLIC, "favicon.ico"),
+    frame: false,
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -59,7 +60,8 @@ async function createWindow() {
   });
 
   if (VITE_DEV_SERVER_URL) {
-    // #298
+    console.log({ VITE_DEV_SERVER_URL });
+
     win.loadURL(VITE_DEV_SERVER_URL);
     // Open devTool if the app is not packaged
     win.webContents.openDevTools();
