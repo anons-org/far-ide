a1688bae4f6b68b1a409ce99b7675f28374293a5
worksapce <289582304@qq.com>
2025-01-17 12:07:49 +0800

    feat: build project

---
+++ vite.config.ts
@@ -0,0 +1,84 @@
+import { rmSync } from "node:fs";
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import electron from "vite-plugin-electron/simple";
+import pkg from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  rmSync("out/far", { recursive: true, force: true });
+
+  const isServe = command === "serve";
+  const isBuild = command === "build";
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+
+  return {
+    root: path.join(__dirname, "src/renderer"),
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/renderer"),
+      },
+    },
+    plugins: [
+      react(),
+      electron({
+        main: {
+          entry: "src/far/main/index.ts",
+          onstart(args) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(
+                /* For `.vscode/.debug.script.mjs` */ "[startup] Electron App"
+              );
+            } else {
+              args.startup();
+            }
+          },
+          vite: {
+            root: path.join(__dirname, "."),
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: "out/far/main",
+              rollupOptions: {
+                external: Object.keys(
+                  "dependencies" in pkg ? pkg.dependencies : {}
+                ),
+              },
+            },
+          },
+        },
+        preload: {
+          input: "src/far/preload/index.ts",
+          vite: {
+            root: path.join(__dirname, "."),
+            build: {
+              sourcemap: sourcemap ? "inline" : undefined, // #332
+              minify: isBuild,
+              outDir: "out/far/preload",
+              rollupOptions: {
+                external: Object.keys(
+                  "dependencies" in pkg ? pkg.dependencies : {}
+                ),
+              },
+            },
+          },
+        },
+        renderer: {},
+      }),
+    ],
+    build: {
+      outDir: "out/renderer",
+    },
+    server:
+      process.env.VSCODE_DEBUG &&
+      (() => {
+        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+        return {
+          host: url.hostname,
+          port: +url.port,
+        };
+      })(),
+    clearScreen: false,
+  };
+});
